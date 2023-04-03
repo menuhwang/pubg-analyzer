c9159aca2cdbbbb1bebc4de30f6eca91b6d0dfab
menuHwang <hmin0206@naver.com>
2023-04-03 20:48:31 +0900

    [Feat] Match 조회 서비스 구현

---
+++ src/main/java/com/menu/pubganalyzer/dao/repository/MatchRepository.java
@@ -0,0 +1,16 @@
+package com.menu.pubganalyzer.dao.repository;
+
+import com.menu.pubganalyzer.model.Match;
+import com.menu.pubganalyzer.model.enums.Shard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+import java.util.Set;
+
+public interface MatchRepository extends JpaRepository<Match, String> {
+    @Query("SELECT m FROM Match m join fetch m.participants join fetch m.rosters join fetch m.asset WHERE m.shardId = :shard AND m.id IN :ids")
+    Set<Match> findByShardIdAndIdIn(Shard shard, Collection<String> ids);
+    @Query("SELECT m FROM Match m join fetch m.participants p join fetch m.rosters join fetch m.asset WHERE m.shardId = :shard AND p.playerId = :playerId  ")
+    Set<Match> findByShardIdAndPlayerId(Shard shard, String playerId);
+}
