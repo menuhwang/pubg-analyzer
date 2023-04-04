af66e721f6736559dceea05341ee65121667253b
menuHwang <hmin0206@naver.com>
2023-04-04 18:33:19 +0900

    [Feat] 매치 검색 컨트롤러 연결

--- src/main/java/com/menu/pubganalyzer/controller/PlayerController.java
+++ src/main/java/com/menu/pubganalyzer/controller/PlayerController.java
@@ -1,7 +1,9 @@
 package com.menu.pubganalyzer.controller;
 
 import com.menu.pubganalyzer.model.Player;
+import com.menu.pubganalyzer.model.dto.MatchRes;
 import com.menu.pubganalyzer.model.enums.Shard;
+import com.menu.pubganalyzer.service.MatchesService;
 import com.menu.pubganalyzer.service.PlayerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,17 +11,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/players")
 @RequiredArgsConstructor
 public class PlayerController {
     private final PlayerService playerService;
+    private final MatchesService matchesService;
 
     @GetMapping()
     public String search(Shard shard, String nickname, Model model) {
         Player player = playerService.getPlayerByNickname(shard, nickname);
+        List<MatchRes> matches = matchesService.getMatch(player);
+
         model.addAttribute("viewTitle", nickname);
         model.addAttribute("player", player);
+        model.addAttribute("matches", matches);
 
         return "player";
     }
