0d4030268bb00354ba28f3c3edf05f0d5f294700
menuHwang <hmin0206@naver.com>
2023-04-07 17:53:09 +0900

    [Feat] 카페인 캐시 설정

---
+++ src/main/java/com/menu/pubganalyzer/config/CacheType.java
@@ -0,0 +1,28 @@
+package com.menu.pubganalyzer.config;
+
+public enum CacheType {
+    RENEW_PLAYERS("renew_players", 10 * 60, 1024),
+    ;
+
+    private final String cacheName;
+    private final int expiredAfterWrite;
+    private final int maximumSize;
+
+    CacheType(String cacheName, int expiredAfterWrite, int maximumSize) {
+        this.cacheName = cacheName;
+        this.expiredAfterWrite = expiredAfterWrite;
+        this.maximumSize = maximumSize;
+    }
+
+    public String getCacheName() {
+        return cacheName;
+    }
+
+    public int getExpiredAfterWrite() {
+        return expiredAfterWrite;
+    }
+
+    public int getMaximumSize() {
+        return maximumSize;
+    }
+}
