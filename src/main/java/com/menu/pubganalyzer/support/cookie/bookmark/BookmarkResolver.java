a4171be3c66ee836332f1b1c0627817ab618df6f
menuHwang <hmin0206@naver.com>
2023-05-30 14:12:05 +0900

    [Feat] BookmarkCookieParameter 작성
    - Cookie와 만료시간, 버전을 관리하기 위한 클래스 생성.
    - BookmarkResolver 수정.

--- src/main/java/com/menu/pubganalyzer/support/cookie/bookmark/BookmarkResolver.java
+++ src/main/java/com/menu/pubganalyzer/support/cookie/bookmark/BookmarkResolver.java
@@ -1,47 +1,30 @@
 package com.menu.pubganalyzer.support.cookie.bookmark;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.springframework.core.MethodParameter;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Type;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
 @Component
 public class BookmarkResolver implements HandlerMethodArgumentResolver {
-    private static final Gson GSON = new Gson();
-    private static final Type BOOKMARK_LIST = new TypeToken<ArrayList<Bookmark>>(){}.getType();
-
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.hasParameterAnnotation(BookmarkCookie.class);
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+    public Object resolveArgument(@NonNull MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         HttpServletRequest httpServletRequest = (HttpServletRequest) webRequest.getNativeRequest();
 
-        Cookie[] cookies = httpServletRequest.getCookies();
-
-        ArrayList<Bookmark> bookmarks = new ArrayList<>();
-
-        if (cookies == null) return bookmarks;
-
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("bookmark")) {
-                bookmarks = GSON.fromJson(URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8), BOOKMARK_LIST);
-            }
+        if (httpServletRequest.getAttribute(BookmarkCookieParameter.class.getName()) instanceof BookmarkCookieParameter) {
+            return httpServletRequest.getAttribute(BookmarkCookieParameter.class.getName());
         }
 
-        return bookmarks;
+        return BookmarkCookieParameter.empty();
     }
 }
