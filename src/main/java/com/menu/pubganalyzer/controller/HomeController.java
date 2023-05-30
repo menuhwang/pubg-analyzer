7551cbfef61ff76f045cfbd3ca168985dc59263b
menuHwang <hmin0206@naver.com>
2023-05-30 14:15:18 +0900

    [Fix] HomeController, PlayerController 수정

--- src/main/java/com/menu/pubganalyzer/controller/HomeController.java
+++ src/main/java/com/menu/pubganalyzer/controller/HomeController.java
@@ -1,23 +1,22 @@
 package com.menu.pubganalyzer.controller;
 
 import com.menu.pubganalyzer.domain.model.enums.Shard;
-import com.menu.pubganalyzer.support.cookie.bookmark.Bookmark;
 import com.menu.pubganalyzer.support.cookie.bookmark.BookmarkCookie;
+import com.menu.pubganalyzer.support.cookie.bookmark.BookmarkCookieParameter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 public class HomeController {
     @GetMapping
-    public String home(@BookmarkCookie List<Bookmark> bookmarks, Model model) {
+    public String home(@BookmarkCookie BookmarkCookieParameter bookmarkCookieParameter, Model model) {
         model.addAttribute("viewTitle", "Home");
         model.addAttribute("shards", Arrays.stream(Shard.values()).map(Shard::name).collect(Collectors.toList()));
-        model.addAttribute("bookmarks", bookmarks);
+        model.addAttribute("bookmarks", bookmarkCookieParameter.getBookmarks());
         return "index";
     }
 }
