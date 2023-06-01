a92f2509d10e7bc6f5b6d85b19fe511bdbb26fe0
menuHwang <hmin0206@naver.com>
2023-06-01 20:24:14 +0900

    fixup! [Refactor] ParticipantDAO 구현

--- src/main/java/com/menu/pubganalyzer/exception/ErrorCode.java
+++ src/main/java/com/menu/pubganalyzer/exception/ErrorCode.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     PLAYER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 플레이어를 찾을 수 없습니다."),
     MATCH_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 매치를 찾을 수 없습니다."),
-    TELEMETRY_FILE_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "로그 파일을 찾을 수 없습니다."),
+    PARTICIPANT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 참가자를 찾을 수 없습니다."),
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러 발생."),
     ;
 
