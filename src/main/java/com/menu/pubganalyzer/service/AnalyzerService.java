25c4629347eb31d474f6eacfeee4b5d7c9045cb7
menuHwang <hmin0206@naver.com>
2023-06-02 18:01:26 +0900

    [Refactor] 매치 결과 분석 기능 리팩토링

--- src/main/java/com/menu/pubganalyzer/service/AnalyzerService.java
+++ src/main/java/com/menu/pubganalyzer/service/AnalyzerService.java
@@ -1,15 +1,14 @@
 package com.menu.pubganalyzer.service;
 
 import com.menu.pubganalyzer.domain.Analyzer;
+import com.menu.pubganalyzer.domain.Telemetry;
+import com.menu.pubganalyzer.domain.dao.MatchDAO;
+import com.menu.pubganalyzer.domain.dao.ParticipantDAO;
+import com.menu.pubganalyzer.domain.dao.TelemetryDAO;
 import com.menu.pubganalyzer.domain.dto.AnalyzerRes;
 import com.menu.pubganalyzer.domain.model.Match;
 import com.menu.pubganalyzer.domain.model.Participant;
 import com.menu.pubganalyzer.domain.model.Roster;
-import com.menu.pubganalyzer.domain.repository.MatchRepository;
-import com.menu.pubganalyzer.domain.repository.ParticipantRepository;
-import com.menu.pubganalyzer.exception.MatchNotFoundException;
-import com.menu.pubganalyzer.exception.PlayerNotFoundException;
-import com.menu.pubganalyzer.facade.TelemetryFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,21 +18,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class AnalyzerService {
-    private final ParticipantRepository participantRepository;
-    private final MatchRepository matchRepository;
-    private final TelemetryFacade telemetryFacade;
+    private final MatchDAO matchDAO;
+    private final ParticipantDAO participantDAO;
+    private final TelemetryDAO telemetryDAO;
 
     @Transactional
     public AnalyzerRes analyze(String matchId, String nickname) {
-        Match match = matchRepository.findById(matchId)
-                .orElseThrow(MatchNotFoundException::new);
-        Participant participant = participantRepository.findByNameAndMatch(nickname, match)
-                .orElseThrow(PlayerNotFoundException::new);
+        Match match = matchDAO.findById(matchId);
+        Participant participant = participantDAO.findByMatchIdAndPlayerName(match.getId(), nickname);
 
-        Roster team = participant.getRoster();
+        Roster roster = participant.getRoster();
 
-        Analyzer teamLog = telemetryFacade.findLogs(match, team);
-        Analyzer analyzer = teamLog.filterOfKiller(nickname);
+        Telemetry rosterTelemetry = telemetryDAO.findByMatchAndRoster(match, roster);
+        Analyzer analyzer = Analyzer.of(rosterTelemetry)
+                .filterOfKiller(nickname);
         return AnalyzerRes.of(analyzer, match.getCreatedAt());
     }
 }
--- src/main/java/com/menu/pubganalyzer/service/FindService.java
+++ src/main/java/com/menu/pubganalyzer/service/FindService.java
@@ -1,27 +1,23 @@
 package com.menu.pubganalyzer.service;
 
+import com.menu.pubganalyzer.domain.dao.MatchDAO;
+import com.menu.pubganalyzer.domain.dao.ParticipantDAO;
 import com.menu.pubganalyzer.domain.model.Match;
 import com.menu.pubganalyzer.domain.model.Participant;
 import com.menu.pubganalyzer.domain.model.Roster;
-import com.menu.pubganalyzer.domain.repository.MatchRepository;
-import com.menu.pubganalyzer.domain.repository.ParticipantRepository;
-import com.menu.pubganalyzer.exception.MatchNotFoundException;
-import com.menu.pubganalyzer.exception.PlayerNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FindService {
-    private final MatchRepository matchRepository;
-    private final ParticipantRepository participantRepository;
+    private final MatchDAO matchDAO;
+    private final ParticipantDAO participantDAO;
 
     public Roster findRosterByMatchIdAndPlayer(String matchId, String nickname) {
-        Match match = matchRepository.findById(matchId)
-                .orElseThrow(MatchNotFoundException::new);
+        Match match = matchDAO.findById(matchId);
 
-        Participant participant = participantRepository.findByNameAndMatch(nickname, match)
-                .orElseThrow(PlayerNotFoundException::new);
+        Participant participant = participantDAO.findByMatchIdAndPlayerName(match.getId(), nickname);
 
         return participant.getRoster();
     }
