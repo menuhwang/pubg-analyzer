27d705a1e4573f7e129928ee4d49f50aa13d23aa
menuHwang <hmin0206@naver.com>
2023-06-02 18:03:48 +0900

    [Remove] 리팩토링으로 인한 불필요한 파일 삭제

--- src/main/java/com/menu/pubganalyzer/exception/TelemetryFileNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.menu.pubganalyzer.exception;
-
-public class TelemetryFileNotFound extends AbstractApplicationException {
-    public TelemetryFileNotFound() {
-        super(ErrorCode.TELEMETRY_FILE_NOT_FOUND);
-    }
-}
