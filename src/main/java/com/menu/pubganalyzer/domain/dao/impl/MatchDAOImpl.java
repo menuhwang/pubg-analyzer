34bb89b50967f4d5b8a4f3f37d73485f42818a90
menuHwang <hmin0206@naver.com>
2023-06-07 15:27:52 +0900

    [Fix] 매치 삭제 로직 수정
    - 캐시된 데이터도 삭제하도록 수정

--- src/main/java/com/menu/pubganalyzer/domain/dao/impl/MatchDAOImpl.java
+++ src/main/java/com/menu/pubganalyzer/domain/dao/impl/MatchDAOImpl.java
@@ -8,27 +8,58 @@ import com.menu.pubganalyzer.exception.MatchNotFoundException;
 import com.menu.pubganalyzer.util.pubgAPI.PubgAPI;
 import com.menu.pubganalyzer.util.pubgAPI.exception.PubgAPIMatchNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.Cache;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 @RequiredArgsConstructor
 public class MatchDAOImpl implements MatchDAO {
     private final MatchRepository matchRepository;
     private final PubgAPI pubgAPI;
+    private final Cache matchCache;
+    private final Cache participantCache;
 
     @Override
-    @Cacheable(cacheNames = "match", key = "#id")
     public Match findById(String id) {
-        Optional<Match> optionalMatch = matchRepository.findByIdFetchParticipant(id);
-        if (optionalMatch.isPresent()) return optionalMatch.get();
+        Match match  = matchCache.get(id, Match.class);
+        if (match != null) return match;
 
-        try {
-            return Match.of(pubgAPI.match(Shard.STEAM.name(), id));
-        } catch (PubgAPIMatchNotFoundException e) {
-            throw new MatchNotFoundException(id);
+        Optional<Match> optionalMatch = matchRepository.findByIdFetchParticipant(id);
+        if (optionalMatch.isPresent()) {
+            match = optionalMatch.get();
+        } else {
+            try {
+                match = Match.of(pubgAPI.match(Shard.STEAM.name(), id));
+            } catch (PubgAPIMatchNotFoundException e) {
+                throw new MatchNotFoundException(id);
+            }
         }
+        matchCache.put(id, match);
+        return match;
+    }
+
+    @Override
+    public Page<Match> findById(String id, Pageable pageable) {
+        return matchRepository.findByIdContains(id, pageable);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        Match match = matchCache.get(id, Match.class);
+        if (match == null) {
+            match = matchRepository.findByIdFetchParticipant(id)
+                    .orElseThrow(MatchNotFoundException::new);
+        }
+        Set<String> playerNames = match.extractPlayerNames();
+        for (String name : playerNames) {
+            participantCache.evict(id + "_" + name);
+        }
+        matchCache.evict(id);
+        matchRepository.deleteById(id);
     }
 }
--- src/main/java/com/menu/pubganalyzer/domain/dao/impl/ParticipantDAOImpl.java
+++ src/main/java/com/menu/pubganalyzer/domain/dao/impl/ParticipantDAOImpl.java
@@ -1,27 +1,21 @@
 package com.menu.pubganalyzer.domain.dao.impl;
 
-import com.menu.pubganalyzer.config.CacheType;
 import com.menu.pubganalyzer.domain.dao.ParticipantDAO;
 import com.menu.pubganalyzer.domain.model.Match;
 import com.menu.pubganalyzer.domain.model.Participant;
 import com.menu.pubganalyzer.domain.repository.ParticipantRepository;
 import com.menu.pubganalyzer.exception.ParticipantNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class ParticipantDAOImpl implements ParticipantDAO {
     private final ParticipantRepository participantRepository;
 
     private final Cache matchCache;
     private final Cache participantCache;
-
-    public ParticipantDAOImpl(ParticipantRepository participantRepository, CacheManager cacheManager) {
-        this.participantRepository = participantRepository;
-        this.matchCache = cacheManager.getCache(CacheType.MATCH.getCacheName());
-        this.participantCache = cacheManager.getCache(CacheType.PARTICIPANT.getCacheName());
-    }
 
     @Override
     public Participant findByMatchIdAndPlayerName(String matchId, String playerName) {
