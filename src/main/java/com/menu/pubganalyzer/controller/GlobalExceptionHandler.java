16ee72f3bc5f007dd20d8e3735d8f7d5ccee64af
menuHwang <hmin0206@naver.com>
2023-06-12 13:37:21 +0900

    [Feat] 잘못된 접근 예외 핸들링
    - ParticipantNotFoundException
    - MatchNotFoundException
    - PlayerNotFoundException

--- src/main/java/com/menu/pubganalyzer/controller/GlobalExceptionHandler.java
+++ src/main/java/com/menu/pubganalyzer/controller/GlobalExceptionHandler.java
@@ -1,13 +1,21 @@
 package com.menu.pubganalyzer.controller;
 
-import com.menu.pubganalyzer.util.pubgAPI.exception.PubgAPIPlayerNotFoundException;
+import com.menu.pubganalyzer.exception.MatchNotFoundException;
+import com.menu.pubganalyzer.exception.ParticipantNotFoundException;
+import com.menu.pubganalyzer.exception.PlayerNotFoundException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(PubgAPIPlayerNotFoundException.class)
-    public String playerNotFoundHandle() {
-        return "/error/player-not-found";
+    @ExceptionHandler(value = {
+            ParticipantNotFoundException.class,
+            MatchNotFoundException.class,
+            PlayerNotFoundException.class
+    })
+    public String badRequestExceptionHandler(Model model) {
+        model.addAttribute("viewTitle", "잘못된 접근");
+        return "error/bad-request";
     }
 }
