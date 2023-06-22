402ad8518335772ad513b5d680ffd51f96026ecd
menuHwang <hmin0206@naver.com>
2023-06-22 15:06:22 +0900

    [Feat] ResponseDTO 생성 및 수정
    생성
    - MatchInfoRes
    - MatchResultRes
    - ReportRes
    
    수정
    - AnalyzerRes
    - DamageLogRes

---
+++ src/main/java/com/menu/pubganalyzer/domain/dto/MatchInfoRes.java
@@ -0,0 +1,37 @@
+package com.menu.pubganalyzer.domain.dto;
+
+import com.menu.pubganalyzer.domain.model.Match;
+import com.menu.pubganalyzer.domain.model.enums.match.GameMode;
+import com.menu.pubganalyzer.domain.model.enums.match.MapName;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class MatchInfoRes {
+    private final String id;
+    private final MapName map;
+    private final GameMode mode;
+    private final LocalDateTime createdAt;
+    private final Integer duration;
+
+    @Builder
+    public MatchInfoRes(String id, MapName map, GameMode mode, LocalDateTime createdAt, Integer duration) {
+        this.id = id;
+        this.map = map;
+        this.mode = mode;
+        this.createdAt = createdAt;
+        this.duration = duration;
+    }
+
+    public static MatchInfoRes of(Match match) {
+        return MatchInfoRes.builder()
+                .id(match.getId())
+                .map(match.getMapName())
+                .mode(match.getGameMode())
+                .createdAt(match.getCreatedAt())
+                .duration(match.getDuration())
+                .build();
+    }
+}
