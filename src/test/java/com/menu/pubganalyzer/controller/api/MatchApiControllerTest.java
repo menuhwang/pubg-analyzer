83f1f003a45fc85c2930261a32a4cf1b7399ee32
menuHwang <hmin0206@naver.com>
2023-06-25 00:08:37 +0900

    [Test] MatchApi deleteById 테스트

--- src/test/java/com/menu/pubganalyzer/controller/api/MatchApiControllerTest.java
+++ src/test/java/com/menu/pubganalyzer/controller/api/MatchApiControllerTest.java
@@ -7,7 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -39,6 +41,23 @@ class MatchApiControllerTest {
                 .andExpect(jsonPath("$.result.content[0].customMatch").isBoolean())
                 .andExpect(jsonPath("$.result.content[0].matchType").isString())
                 .andExpect(jsonPath("$.result.content[0].createdAt").isString())
+        ;
+    }
+
+    @Transactional
+    @Test
+    void deleteById() throws Exception {
+        String id = "069990ee-ce9a-43b4-9621-8da0339b4adf";
+        ResultActions result = mockMvc.perform(
+                delete("/api/matches/" + id)
+        );
+
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(MatchApiController.class))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.status").value(200))
+                .andExpect(jsonPath("$.result.id").value(id))
         ;
     }
 }
