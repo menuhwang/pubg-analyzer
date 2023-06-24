eccc7930c7a6914619f897974b5e1e9a2853167e
menuHwang <hmin0206@naver.com>
2023-06-24 23:53:37 +0900

    [Test] MatchApi findAll 메소드 테스트

--- src/main/java/com/menu/pubganalyzer/support/admin/AdminConfig.java
+++ src/main/java/com/menu/pubganalyzer/support/admin/AdminConfig.java
@@ -1,13 +1,11 @@
 package com.menu.pubganalyzer.support.admin;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class AdminConfig {
-    private final AdminProperties adminProperties;
+    private final AdminProperties adminProperties = new AdminProperties();
 
     @Bean
     public AdminInterceptor adminInterceptor() {
---
+++ src/test/java/com/menu/pubganalyzer/controller/api/MatchApiControllerTest.java
@@ -0,0 +1,44 @@
+package com.menu.pubganalyzer.controller.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class MatchApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void findAll() throws Exception {
+        ResultActions result = mockMvc.perform(
+                get("/api/matches")
+                        .param("page", "0")
+                        .param("size", "20")
+        );
+
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(MatchApiController.class))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.status").value(200))
+                .andExpect(jsonPath("$.result.content[0].id").isString())
+                .andExpect(jsonPath("$.result.content[0].gameMode").isString())
+                .andExpect(jsonPath("$.result.content[0].duration").isNumber())
+                .andExpect(jsonPath("$.result.content[0].mapName").isString())
+                .andExpect(jsonPath("$.result.content[0].customMatch").isBoolean())
+                .andExpect(jsonPath("$.result.content[0].matchType").isString())
+                .andExpect(jsonPath("$.result.content[0].createdAt").isString())
+        ;
+    }
+}
