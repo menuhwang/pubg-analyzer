cfc3f739b12a687b5aaa8cddef9747e2f7a08066
menuHwang <hmin0206@naver.com>
2023-06-25 12:48:52 +0900

    [Test] FetchPubgApiController 테스트
    - fetchPlayer
    - fetchMatch

---
+++ src/test/java/com/menu/pubganalyzer/controller/api/FetchPubgApiControllerTest.java
@@ -0,0 +1,71 @@
+package com.menu.pubganalyzer.controller.api;
+
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class FetchPubgApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static final String SHARD = "STEAM";
+    private static final String PLAYER_NICKNAME = "WackyJacky101";
+    private static String MATCH_ID;
+
+    @Test
+    @Order(1)
+    void fetchPlayer() throws Exception {
+        ResultActions result = mockMvc.perform(
+                get("/api/fetch/player")
+                        .param("shard", SHARD)
+                        .param("nickname", PLAYER_NICKNAME)
+        );
+
+        result.andDo(print())
+                .andExpect(handler().handlerType(FetchPubgApiController.class))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.status").value(200))
+                .andExpect(jsonPath("$.result.data").isArray())
+                .andExpect(jsonPath("$.result.data.length()").value(1))
+                .andExpect(jsonPath("$.result.data[0].attributes.name").value(PLAYER_NICKNAME))
+                .andExpect(jsonPath("$.result.data[0].relationships.matches.data").isArray())
+        ;
+
+        MATCH_ID = JsonPath.read(result.andReturn().getResponse().getContentAsString(), "$.result.data[0].relationships.matches.data[0].id");
+    }
+
+    @Test
+    @Order(2)
+    void fetchMatch() throws Exception {
+        ResultActions result = mockMvc.perform(
+                get("/api/fetch/match")
+                        .param("shard", SHARD)
+                        .param("id", MATCH_ID)
+        );
+
+        result.andDo(print())
+                .andExpect(handler().handlerType(FetchPubgApiController.class))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.status").value(200))
+                .andExpect(jsonPath("$.result.data.id").value(MATCH_ID))
+                .andExpect(jsonPath("$.result.included").isArray())
+        ;
+    }
+}
