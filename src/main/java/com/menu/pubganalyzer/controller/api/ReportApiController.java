df4dcb8996972200973bbc05372eb7323fe66c07
menuHwang <hmin0206@naver.com>
2023-07-05 13:51:35 +0900

    [Feat] Report REST API 구현

---
+++ src/main/java/com/menu/pubganalyzer/controller/api/ReportApiController.java
@@ -0,0 +1,32 @@
+package com.menu.pubganalyzer.controller.api;
+
+import com.menu.pubganalyzer.domain.dto.ReportRes;
+import com.menu.pubganalyzer.service.ReportService;
+import com.menu.pubganalyzer.support.apiResult.ApiResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+import static com.menu.pubganalyzer.support.apiResult.ApiResultUtil.success;
+
+@RestController
+@RequestMapping("/api/admin/v2/report")
+@RequiredArgsConstructor
+public class ReportApiController {
+    private final ReportService reportService;
+
+    @GetMapping("/match/{matchId}/player/{nickname}")
+    public ResponseEntity<ApiResult<ReportRes>> getMatchReport(
+            @Valid @NotBlank @PathVariable String matchId,
+            @Valid @NotBlank @PathVariable String nickname) {
+        ReportRes report = reportService.report(matchId, nickname);
+
+        return ResponseEntity.ok(success(report));
+    }
+}
