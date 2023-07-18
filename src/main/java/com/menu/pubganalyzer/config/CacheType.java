d220da36dba6c942ddc33d887cd00dd87cfeea4a
menuHwang <hmin0206@naver.com>
2023-07-18 11:54:42 +0900

    [Fix] 캐시 사이즈, 만료 시간 변경

--- src/main/java/com/menu/pubganalyzer/config/CacheType.java
+++ src/main/java/com/menu/pubganalyzer/config/CacheType.java
@@ -1,12 +1,12 @@
 package com.menu.pubganalyzer.config;
 
 public enum CacheType {
-    PUBG_API_TELEMETRY("pubg_api_telemetry", 60, 10000),
-    MATCH("match", 30 * 60, 2000),
+    PUBG_API_TELEMETRY("pubg_api_telemetry", 10, 5),
+    MATCH("match", 30 * 60, 20 * 30),
     PLAYER("player", 60, 20),
-    PARTICIPANT("participant", 30 * 60, 2000 * 20),
-    TELEMETRY("telemetry", 30 * 60, 2000),
-    ROSTER_TELEMETRY("roster_telemetry", 30 * 60, 2000 * 20),
+    PARTICIPANT("participant", 5 * 60, 20 * 30 * 100),
+    TELEMETRY("telemetry", 5 * 60, 20 * 30),
+    ROSTER_TELEMETRY("roster_telemetry", 5 * 60, 20 * 30 * 25),
     ;
 
     private final String cacheName;
