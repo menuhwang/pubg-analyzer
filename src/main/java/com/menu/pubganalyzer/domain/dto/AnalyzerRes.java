caebee0d805895ca7b7fa778df5d3d03de977fcd
menuHwang <hmin0206@naver.com>
2023-07-31 10:25:36 +0900

    [Style] 빌더 어노테이션 추가

--- src/main/java/com/menu/pubganalyzer/domain/dto/AnalyzerRes.java
+++ src/main/java/com/menu/pubganalyzer/domain/dto/AnalyzerRes.java
@@ -2,6 +2,7 @@ package com.menu.pubganalyzer.domain.dto;
 
 import com.menu.pubganalyzer.domain.Analyzer;
 import com.menu.pubganalyzer.util.ChartUtil;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,6 +10,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class AnalyzerRes {
     private final List<KillLogRes> killLog;
     private final List<DamageLogRes> damageLog;
@@ -17,24 +20,6 @@ public class AnalyzerRes {
     private final Map<String, Map<String, Float>> victimPlayerDamageDealt;
     private final Map<String, List<DamageLogRes>> victimDamageLog;
     private final Map<String, Object> charts;
-
-    @Builder
-    public AnalyzerRes(
-            List<KillLogRes> killLog,
-            List<DamageLogRes> damageLog,
-            Integer player,
-            Integer bot,
-            Map<String, Map<String, Float>> victimPlayerDamageDealt,
-            Map<String, List<DamageLogRes>> victimDamageLog,
-            Map<String, Object> charts) {
-        this.killLog = killLog;
-        this.damageLog = damageLog;
-        this.player = Objects.requireNonNullElse(player, 0);
-        this.bot = Objects.requireNonNullElse(bot, 0);
-        this.victimPlayerDamageDealt = victimPlayerDamageDealt;
-        this.victimDamageLog = victimDamageLog;
-        this.charts = charts;
-    }
 
     public static AnalyzerRes of(Analyzer analyzer) {
         Map<String, List<DamageLogRes>> map = new HashMap<>();
@@ -53,8 +38,8 @@ public class AnalyzerRes {
         return AnalyzerRes.builder()
                 .killLog(analyzer.getLogPlayerKills().stream().map(KillLogRes::of).collect(Collectors.toList()))
                 .damageLog(analyzer.getTotalLogPlayerTakeDamages().stream().map(DamageLogRes::of).collect(Collectors.toList()))
-                .player(analyzer.countVictimType().get("player"))
-                .bot(analyzer.countVictimType().get("bot"))
+                .player(analyzer.countVictimType().getOrDefault("player", 0))
+                .bot(analyzer.countVictimType().getOrDefault("bot", 0))
                 .victimPlayerDamageDealt(analyzer.analysisDamageDealt())
                 .victimDamageLog(map)
                 .charts(charts)
