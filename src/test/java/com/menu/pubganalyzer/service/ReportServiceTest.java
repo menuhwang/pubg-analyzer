560e26427a46a8b775a24b39c51cee72deb0d032
menuHwang <hmin0206@naver.com>
2023-08-09 10:47:08 +0900

    [Refactor] Telemetry 파일 존재 확인 및 fetch 코드 중복 제거

--- src/test/java/com/menu/pubganalyzer/service/ReportServiceTest.java
+++ src/test/java/com/menu/pubganalyzer/service/ReportServiceTest.java
@@ -11,6 +11,7 @@ import static com.menu.pubganalyzer.support.fixture.PlayerFixture.PLAYER_NAME;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 class ReportServiceTest {
@@ -19,6 +20,8 @@ class ReportServiceTest {
 
     @Test
     void getMatchReport() {
+        given(telemetryService.existsTelemetry(MATCH))
+                .willReturn(true);
         given(telemetryService.findKillLogs(any(Match.class), eq(PLAYER_NAME)))
                 .willReturn(OFFICIAL_LOG_PLAYER_KILLS);
         given(telemetryService.findDamageLogs(any(Match.class),  anyCollection(), anyCollection()))
@@ -28,6 +31,28 @@ class ReportServiceTest {
 
         assertDoesNotThrow(() -> reportService.getMatchReport(MATCH, PLAYER_NAME));
 
+        verify(telemetryService).existsTelemetry(MATCH);
+        verify(telemetryService, never()).fetchTelemetry(any());
+        verify(telemetryService).findKillLogs(MATCH, PLAYER_NAME);
+        verify(telemetryService).findDamageLogs(any(Match.class), anyCollection(), anyCollection());
+        verify(telemetryService).findDamageLogs(MATCH, PLAYER_NAME);
+    }
+
+    @Test
+    void getMatchReportWithFetch() {
+        given(telemetryService.existsTelemetry(MATCH))
+                .willReturn(false);
+        given(telemetryService.findKillLogs(any(Match.class), eq(PLAYER_NAME)))
+                .willReturn(OFFICIAL_LOG_PLAYER_KILLS);
+        given(telemetryService.findDamageLogs(any(Match.class),  anyCollection(), anyCollection()))
+                .willReturn(OFFICIAL_LOG_PLAYER_TAKE_DAMAGES);
+        given(telemetryService.findDamageLogs(any(Match.class),  eq(PLAYER_NAME)))
+                .willReturn(OFFICIAL_LOG_PLAYER_TAKE_DAMAGES);
+
+        assertDoesNotThrow(() -> reportService.getMatchReport(MATCH, PLAYER_NAME));
+
+        verify(telemetryService).existsTelemetry(MATCH);
+        verify(telemetryService).fetchTelemetry(MATCH);
         verify(telemetryService).findKillLogs(MATCH, PLAYER_NAME);
         verify(telemetryService).findDamageLogs(any(Match.class), anyCollection(), anyCollection());
         verify(telemetryService).findDamageLogs(MATCH, PLAYER_NAME);
