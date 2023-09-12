2011ffa4afe2f764ae5b5e88ff137836b6d00415
menuHwang <hmin0206@naver.com>
2023-09-12 19:26:24 +0900

    [Refactor] 레거시 삭제

--- src/main/java/com/menu/pubganalyzer/util/ChartUtil.java
+++ src/main/java/com/menu/pubganalyzer/util/ChartUtil.java
@@ -12,65 +12,6 @@ import java.util.stream.Collectors;
 public class ChartUtil {
     private static final int PHASE_SIZE = 10; // 0 ~ 9
 
-    public static Map<String, Object> phaseDamageChart(Collection<LogPlayerTakeDamage> logPlayerTakeDamages) {
-        Map<String, Object> result = new HashMap<>();
-
-        float[] phaseDamageDealt = new float[PHASE_SIZE];
-
-        for (LogPlayerTakeDamage logPlayerTakeDamage : logPlayerTakeDamages) {
-            int phase = logPlayerTakeDamage.getPhase();
-            phaseDamageDealt[phase] += logPlayerTakeDamage.getDamage();
-        }
-
-        result.put("datasets", Map.of("data", phaseDamageDealt));
-
-        return result;
-    }
-
-    public static Map<String, Object> contributeDamageChart(List<LogPlayerKillV2> logPlayerKills, Map<String, Map<String, Float>> victimPlayerDamageDealt) {
-        Map<String, Object> result = new HashMap<>();
-
-        String player = logPlayerKills.isEmpty() ? "" : logPlayerKills.get(0).getKillerName();
-
-        List<String> victims = new ArrayList<>();
-        for (LogPlayerKillV2 logPlayerKill : logPlayerKills) {
-            victims.add(logPlayerKill.getVictimName());
-        }
-        result.put("labels", victims);
-
-        Set<String> roster = new HashSet<>();
-        for (String victim : victims) {
-            roster.addAll(victimPlayerDamageDealt.get(victim).keySet());
-        }
-
-        LinkedList<Map<String, Object>> datasets = new LinkedList<>();
-        for (String member : roster) {
-            Map<String, Object> dataset = new HashMap<>();
-            dataset.put("label", member);
-            Float[] data = new Float[victims.size()];
-            for (int i = 0; i < data.length; i++) {
-                data[i] = victimPlayerDamageDealt.get(victims.get(i)).getOrDefault(member, 0F);
-            }
-            dataset.put("data", Arrays.stream(data).collect(Collectors.toList()));
-
-            datasets.add(dataset);
-        }
-
-        Iterator<Map<String, Object>> iterator = datasets.iterator();
-        while (iterator.hasNext()) {
-            Map<String, Object> data = iterator.next();
-            if (player.equals(data.get("label"))) { // 조회한 유저의 데이터를 가장 앞으로 이동한다.
-                iterator.remove();
-                datasets.addFirst(data);
-                break;
-            }
-        }
-
-        result.put("datasets", datasets);
-
-        return result;
-    }
-
     public static PhaseDamageChartRes phaseDamageChart(final List<LogPlayerTakeDamage> logPlayerTakeDamages) {
         float[] phaseDamageDealt = new float[PHASE_SIZE];
 
