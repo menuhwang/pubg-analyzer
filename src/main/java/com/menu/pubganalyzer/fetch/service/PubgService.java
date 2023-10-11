6a92ecc5cb22007cb1e82a87339ac3525a0bd4ec
menuHwang <hmin0206@naver.com>
2023-10-11 19:23:39 +0900

    [Refactor] Telemetry Response 변경으로 인한 import 주소 수정

--- src/main/java/com/menu/pubganalyzer/fetch/service/PubgService.java
+++ src/main/java/com/menu/pubganalyzer/fetch/service/PubgService.java
@@ -7,7 +7,7 @@ import com.menu.pubganalyzer.telemetries.model.Telemetry;
 import com.menu.pubganalyzer.util.pubgAPI.PubgAPI;
 import com.menu.pubganalyzer.util.pubgAPI.response.match.MatchResponse;
 import com.menu.pubganalyzer.util.pubgAPI.response.player.PlayersResponse;
-import com.menu.pubganalyzer.util.pubgAPI.response.TelemetryResponse;
+import com.menu.pubganalyzer.util.pubgAPI.response.telemetry.TelemetryResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
