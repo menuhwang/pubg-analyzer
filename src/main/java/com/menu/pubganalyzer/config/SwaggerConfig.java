49b38b9c9abd8f72212c46975089da3ed386732d
menuHwang <hmin0206@naver.com>
2024-01-09 16:12:01 +0900

    Swagger server url 설정

--- src/main/java/com/menu/pubganalyzer/config/SwaggerConfig.java
+++ src/main/java/com/menu/pubganalyzer/config/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.menu.pubganalyzer.config;
 
 import com.menu.pubganalyzer.support.admin.AdminOnly;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
@@ -8,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(servers = {@Server(url = "${springdoc.server.url}")})
 public class SwaggerConfig {
     @Bean
     public GroupedOpenApi publicApi() {
