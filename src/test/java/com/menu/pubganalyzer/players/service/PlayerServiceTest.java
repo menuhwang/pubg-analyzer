4e5d86d7e7ca67555281c56bec357cd4c32c8ebc
menuHwang <hmin0206@naver.com>
2024-01-30 15:08:25 +0900

    [Test] 플레이어 전적갱신 시 업데이트 카운트가 증가하지 않는 버그 수정

--- src/test/java/com/menu/pubganalyzer/players/service/PlayerServiceTest.java
+++ src/test/java/com/menu/pubganalyzer/players/service/PlayerServiceTest.java
@@ -2,16 +2,19 @@ package com.menu.pubganalyzer.players.service;
 
 import com.menu.pubganalyzer.fetch.service.PubgService;
 import com.menu.pubganalyzer.matches.repository.MatchRepository;
+import com.menu.pubganalyzer.players.model.Player;
 import com.menu.pubganalyzer.players.repository.PlayerRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.menu.pubganalyzer.support.fixture.MatchFixture.MATCH;
 import static com.menu.pubganalyzer.support.fixture.PlayerFixture.PLAYER;
 import static com.menu.pubganalyzer.support.fixture.PlayerFixture.PLAYER_NAME;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -29,6 +32,31 @@ class PlayerServiceTest {
     );
 
     @Test
+    void updateMatchHistoryFirstTime() {
+        given(pubgService.fetchPlayer(PLAYER_NAME))
+                .willReturn(PLAYER);
+
+        when(matchRepository.existsById(anyString()))
+                .thenReturn(false);
+
+        when(pubgService.fetchMatches(anyCollection()))
+                .thenReturn(List.of(MATCH));
+
+        when(playerRepository.findByName(anyString()))
+                .thenReturn(Optional.empty());
+
+        assertDoesNotThrow(() -> playerService.updateMatchHistory(PLAYER_NAME));
+
+        verify(pubgService).fetchPlayer(PLAYER_NAME);
+        verify(matchRepository).existsById(anyString());
+        verify(pubgService).fetchMatches(anyCollection());
+
+        verify(playerRepository).save(any());
+        verify(matchRepository).saveAll(anyCollection());
+    }
+
+
+    @Test
     void updateMatchHistory() {
         given(pubgService.fetchPlayer(PLAYER_NAME))
                 .willReturn(PLAYER);
@@ -39,11 +67,21 @@ class PlayerServiceTest {
         when(pubgService.fetchMatches(anyCollection()))
                 .thenReturn(List.of(MATCH));
 
+        Player updated = Player.builder()
+                .updateCount(3)
+                .name(PLAYER_NAME)
+                .build();
+
+        when(playerRepository.findByName(anyString()))
+                .thenReturn(Optional.of(updated));
+
         assertDoesNotThrow(() -> playerService.updateMatchHistory(PLAYER_NAME));
 
         verify(pubgService).fetchPlayer(PLAYER_NAME);
         verify(matchRepository).existsById(anyString());
         verify(pubgService).fetchMatches(anyCollection());
+
+        assertEquals(4, updated.getUpdateCount());
 
         verify(playerRepository).save(any());
         verify(matchRepository).saveAll(anyCollection());
